37f0f90f61c0fb8bd5f4df80c0762cf61e48848c
GiovanniCalia <johnca@outlook.it>
2022-04-02 19:13:25 +0200

    Completamento header

--- src/components/HeaderSeo.vue
+++ src/components/HeaderSeo.vue
@@ -1,7 +1,8 @@
 
 <template>
 <header>
-    <nav class='d-flex align-items-center justify-content-between py-3 px-3'>
+  <div class="cont_box">
+    <nav class='d-flex align-items-center justify-content-between py-3 px-5'>
         <img src="../../src/assets/images/logo_seo_w_1x.png" alt="logo seo">
         <div class="d-flex align-items-center">
             <ul class='d-flex mt-3 mx-2' v-for='element in listHeader' :key='element.name'>
@@ -11,6 +12,13 @@
             <button class="ms-5">get in touch now</button>
         </div>
     </nav>
+    <div class="box-header px-5">
+      <h1>SEM Campaigns Made Simple With Avada</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Phasellus eu ornare erat. Curabitur pulvinar elit.</p>
+      <button class="me-3">buy avada now</button>
+      <button class="color-blue">contact us</button>
+    </div>
+  </div>
 </header>
 </template>
 
@@ -51,23 +59,22 @@ header{
     background-size: cover;
 
     nav{
-        max-width: 1500px;
-        margin: auto;
-    }
+        font-size: 90%;
 
-    button{
+        button{
         text-transform: uppercase;
-        padding: 0.5rem 1rem;
+        padding: 0.6rem 1.5rem;
         color: white;
-        background-color: orange;
+        background-color: #ffa837;
         border-radius: 100px;
         border: none;
+        }
     }
 
     .button-apply{
       padding: 0.2rem 0.5rem;
-      color: orange;
-      border: 2px solid orange;
+      color: #ffa837;
+      border: 2px solid #ffa837;
       border-radius: 8px;
     }
 
@@ -75,9 +82,34 @@ header{
         list-style-type: none;
 
         a{
-            color: white;
+            color: #f8f8f8;
             text-decoration: none;
+
+            &:hover{
+              color: #ffa837;
+            }
         }
+    }
+
+    .box-header{
+      width: 550px;
+      color: #f8f8f8;
+      padding-top: 5rem;
+      padding-bottom: 10rem;
+
+      button{
+        text-transform: uppercase;
+        padding: 0.6rem 1.8rem;
+        color: #f8f8f8;
+        background-color: #ffa837;
+        border-radius: 100px;
+        border: none;
+        font-size: 80%;
+      }
+
+      .color-blue{
+        background-color: #0369d7;
+      }
     }
 }
 
